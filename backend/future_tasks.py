5426b6806838ab205a0b4710b2887928ce6e9054
verumlotus <universumtremendum@gmail.com>
2022-07-06 16:53:02 -0400

    scaffold for getImageURI, save WIP

--- backend/future_tasks.py
+++ backend/future_tasks.py
@@ -1,3 +1,4 @@
+from tracemalloc import start
 from web3 import Web3
 from dotenv import load_dotenv
 import os
@@ -24,16 +25,6 @@ def getContractNameAndTotalSupply(contract_addr: str) -> dict:
         dict: 'name' of the contract if present (else empty string) and the 'totalSupply'
     """
     # Use the alchemy NFT enchanced API
-    # server_res = requests.post(
-    #     ALCHEMY_URL,
-    #     allow_redirects=True,
-    #     json={
-    #         "jsonrpc": "2.0",
-    #         "method": "alchemy_getTokenMetadata",
-    #         "params": [f'{contract_addr}'],
-    #         "id": 42
-    #     }
-    # )
     server_res = requests.get(
         f'{ALCHEMY_URL}/getContractMetadata',
         params={
@@ -49,17 +40,52 @@ def getContractNameAndTotalSupply(contract_addr: str) -> dict:
         'totalSupply': res_json['totalSupply']
     }
 
-def getMetadataURIs(contract_addr: str) -> list[str]:
-    """Given a contract address for an NFT, this function will fetch the metadataURIs for 
+def getImageURIs(contract_addr: str) -> list[tuple[str, str]]:
+    """Given a contract address for an NFT, this function will fetch the imageURIs for 
     every NFT in the collection
 
     Args:
         contract_addr (str): Ethereum address of the NFT collection
 
     Returns:
-        list[str]: A list of all the metadata URIs for each NFT in the collection
+        list[tuple[str, str]]: A list of all the image URIs for each NFT in the collection where each elemnt 
+        is a tuple of the form (tokenId: str, ImageURI: str)
     """
+    imageURI_list = list()
+    # N.B: Enhanced API below only returns up to 100 NFTs at a time, 
+    # so we need to paginate through until we get all of the NFTs
+    startToken = 0
+    while True:
+        server_res = requests.get(
+            f'{ALCHEMY_URL}/getNFTsForCollection',
+            params={
+                'contractAddress': contract_addr,
+                'withMetadata': "true",
+                'startToken': startToken
+            },
+            allow_redirects=True
+        )
+        
+        if server_res.status_code != 200 or not server_res.json():
+            return ''
+        res_json = server_res.json()
+        # Get the image URIs for all the NFTs returned in this response
+        for nftData in res_json['nfts']:
+            # TODO: Convert this hex into a decimal number
+            tokenId = nftData['id']['tokenId']
+            imageUri = nftData['media'][0]['raw']
+            imageURI_list.append((tokenId, imageUri))
 
+        # If no next token then we are done
+        if not res_json['nextToken']:
+            break
+
+        #TODO: update start Token (need to convert to decimal number from hex string)
+        startToken = res_json['nextToken']
+
+        # TODO: Get rid of this break, it's here only for testing
+        # TODO: need to implement pagination
+        break
+    return imageURI_list
     
-c = getContractNameAndTotalSupply("0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D")
-print(c)
+getMetadataURIs("0x19b86299c21505cdf59cE63740B240A9C822b5E4")
